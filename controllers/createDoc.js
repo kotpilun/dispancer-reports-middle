727b7a61910dbd0809e01091d8367ea176005df0
Oleg Kruglyakov <kotpilun3m@gmail.com>
2024-09-23 16:58:59 +0300

    fix turnir

--- controllers/createDoc.js
+++ controllers/createDoc.js
@@ -1,82 +1,113 @@
-import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-import { modifyDateOfBirth } from '../utils/modifyDateOfBirth.js';
-import { modifySportsCategory } from '../utils/modifySportsCategory.js';
-import { generateChampionshipReport } from '../report_generators/championship.js';
-import { createZipArchive } from '../utils/createZip.js';
-import { sendAndDeleteZipFile } from '../utils/sendAndDeleteZipFile.js';
-import { deleteTemporaryFiles } from '../utils/deleteTemporaryFiles.js';
-import { generateRuzaCupReport } from '../report_generators/ruzaCup.js';
-import { generateOpenChampionshipReport } from '../report_generators/openChampionship.js';
-import { generateGoldenFishReport } from '../report_generators/goldenFish.js';
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { modifyDateOfBirth } from "../utils/modifyDateOfBirth.js";
+import { modifySportsCategory } from "../utils/modifySportsCategory.js";
+import { generateChampionshipReport } from "../report_generators/championship.js";
+import { createZipArchive } from "../utils/createZip.js";
+import { sendAndDeleteZipFile } from "../utils/sendAndDeleteZipFile.js";
+import { deleteTemporaryFiles } from "../utils/deleteTemporaryFiles.js";
+import { generateRuzaCupReport } from "../report_generators/ruzaCup.js";
+import { generateOpenChampionshipReport } from "../report_generators/openChampionship.js";
+import { generateGoldenFishReport } from "../report_generators/goldenFish.js";
 
 // Получаем путь к текущему модулю
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export async function createDoc(req, res) {
-    const dispancers = new Set();
-    console.log('req.body', req.body);
+  const dispancers = new Set();
+  console.log("req.body", req.body);
 
-    // Забираем из тела запроса уникальный список диспансеров
-    if (Array.isArray(req.body?.childInfo) && req.body?.childInfo.length > 0) {
-        req.body.childInfo.forEach(item => {
-            dispancers.add(item.dispancer); 
-        });
-    }
+  // Забираем из тела запроса уникальный список диспансеров
+  if (Array.isArray(req.body?.childInfo) && req.body?.childInfo.length > 0) {
+    req.body.childInfo.forEach((item) => {
+      dispancers.add(item.dispancer);
+    });
+  }
 
-    const childInfo = req.body.childInfo;
-    childInfo.forEach(item => item.dateOfBirth = modifyDateOfBirth(item.dateOfBirth));
-    
-    const createdFiles = [];
-    
-    // Для каждого диспансера создаем отдельный Excel-файл
-    for (let dispancer of dispancers) {
-        try {
-            let filePath; // Объявляем переменную filePath вне блока switch
-            
-            switch (req.body.competitonType) {
-                case 'Чемпионат':
-                    childInfo.forEach(item => item.sportsCategory = modifySportsCategory(item));
-                    filePath = await generateChampionshipReport(dispancer, childInfo, req.body.year, __dirname);
-                    break;
-                    case 'Ruza Cup':
-                        filePath = await generateRuzaCupReport(dispancer, childInfo, req.body.year, req.body.dateOfCompetition, __dirname);
-                        break;
-                    case 'Открытый турнир':
-                        filePath = await generateOpenChampionshipReport(dispancer, childInfo, req.body.year, req.body.dateOfCompetition, req.body.city, req.body.give, __dirname);
-                        break;
-                    case 'Золотая рыбка':
-                        childInfo.forEach(item => item.sportsCategory = modifySportsCategory(item));
-                        filePath = await generateGoldenFishReport(dispancer, childInfo, req.body.year, req.body.girlsBirthYears, req.body.boysBirthYears, req.body.stage, __dirname);
-                        break;
-                default:
-                    throw new Error('Unsupported competition type');
-            }
+  const childInfo = req.body.childInfo;
+  childInfo.forEach(
+    (item) => (item.dateOfBirth = modifyDateOfBirth(item.dateOfBirth))
+  );
 
-            // Добавляем файл в список созданных файлов
-            createdFiles.push(filePath);
+  const createdFiles = [];
 
-        } catch (error) {
-            console.error('Error creating Excel file:', error);
-            res.status(500).send('An error occurred while creating the files');
-            return;
-        }
-    }
-
-    // Создаем ZIP-архив
-    const zipFilePath = path.join(__dirname, 'dispancers_reports.zip');
-
+  // Для каждого диспансера создаем отдельный Excel-файл
+  for (let dispancer of dispancers) {
     try {
-        await createZipArchive(createdFiles, zipFilePath);
+      let filePath; // Объявляем переменную filePath вне блока switch
 
-        sendAndDeleteZipFile(res, zipFilePath);
+      switch (req.body.competitonType) {
+        case "Чемпионат":
+          childInfo.forEach(
+            (item) => (item.sportsCategory = modifySportsCategory(item))
+          );
+          filePath = await generateChampionshipReport(
+            dispancer,
+            childInfo,
+            req.body.year,
+            __dirname
+          );
+          break;
+        case "Ruza Cup":
+          filePath = await generateRuzaCupReport(
+            dispancer,
+            childInfo,
+            req.body.year,
+            req.body.dateOfCompetition,
+            __dirname
+          );
+          break;
+        case "Турнир":
+          filePath = await generateOpenChampionshipReport(
+            dispancer,
+            childInfo,
+            req.body.year,
+            req.body.dateOfCompetition,
+            req.body.city,
+            req.body.give,
+            __dirname
+          );
+          break;
+        case "Золотая рыбка":
+          childInfo.forEach(
+            (item) => (item.sportsCategory = modifySportsCategory(item))
+          );
+          filePath = await generateGoldenFishReport(
+            dispancer,
+            childInfo,
+            req.body.year,
+            req.body.girlsBirthYears,
+            req.body.boysBirthYears,
+            req.body.stage,
+            __dirname
+          );
+          break;
+        default:
+          throw new Error("Unsupported competition type");
+      }
 
-        deleteTemporaryFiles(createdFiles);
-
+      // Добавляем файл в список созданных файлов
+      createdFiles.push(filePath);
     } catch (error) {
-        console.error('Error creating ZIP archive:', error);
-        res.status(500).send('An error occurred while creating the ZIP archive');
+      console.error("Error creating Excel file:", error);
+      res.status(500).send("An error occurred while creating the files");
+      return;
     }
+  }
+
+  // Создаем ZIP-архив
+  const zipFilePath = path.join(__dirname, "dispancers_reports.zip");
+
+  try {
+    await createZipArchive(createdFiles, zipFilePath);
+
+    sendAndDeleteZipFile(res, zipFilePath);
+
+    deleteTemporaryFiles(createdFiles);
+  } catch (error) {
+    console.error("Error creating ZIP archive:", error);
+    res.status(500).send("An error occurred while creating the ZIP archive");
+  }
 }
--- report_generators/openChampionship.js
+++ report_generators/openChampionship.js
@@ -1,5 +1,5 @@
-import ExcelJS from 'exceljs';
-import path from 'path';
+import ExcelJS from "exceljs";
+import path from "path";
 
 /**
  * Функция для создания Excel файла для диспансера
@@ -9,53 +9,65 @@ import path from 'path';
  * @param {string} dirPath - Директория, куда сохранять файлы
  * @returns {Promise<string>} - Путь к созданному файлу
  */
-export async function generateOpenChampionshipReport(dispancer, childInfo, year, dateOfCompetition, city, give, dirPath) {
-    try {
-        const workbook = new ExcelJS.Workbook();
-        
-        // Чтение существующего файла
-        await workbook.xlsx.readFile('./reports/turnir.xlsx');
-        const worksheet = workbook.getWorksheet(1);
+export async function generateOpenChampionshipReport(
+  dispancer,
+  childInfo,
+  year,
+  dateOfCompetition,
+  city,
+  give,
+  dirPath
+) {
+  try {
+    const workbook = new ExcelJS.Workbook();
 
-        let cell = worksheet.getRow(4).getCell('A');
-        cell.value = `г. ${city}, ${dateOfCompetition} ${year} года`;
+    // Чтение существующего файла
+    await workbook.xlsx.readFile("./reports/turnir.xlsx");
+    const worksheet = workbook.getWorksheet(1);
 
-        cell = worksheet.getRow(2).getCell('A');
-        cell.value = `на участие в Открытом турнире по плаванию, посвященного "${give}"`
+    let cell = worksheet.getRow(4).getCell("A");
+    cell.value = `г. ${city}, ${dateOfCompetition} ${year} года`;
 
-        // Вставка новых строк для каждого ребенка из текущего диспансера
-        let insertRowIndex = 6; // Индекс строки, начиная с которой вставлять
-        const filteredChildren = childInfo.filter(item => item.dispancer === dispancer);
+    cell = worksheet.getRow(2).getCell("A");
+    cell.value = give;
 
-        filteredChildren.forEach((value, index) => {
-            const row = worksheet.insertRow(insertRowIndex + index, [
-                index + 1, 
-                `${value.surname} ${value.name} ${value.secondName}`, 
-                value.dateOfBirth,
-                value.sportsCategory,
-                'Атлант — Домодедово',
-                'Якуш Т. Д.'
-            ]);
+    // Вставка новых строк для каждого ребенка из текущего диспансера
+    let insertRowIndex = 6; // Индекс строки, начиная с которой вставлять
+    const filteredChildren = childInfo.filter(
+      (item) => item.dispancer === dispancer
+    );
 
-            row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
-                if (colNumber !== 1) {
-                    cell.alignment = { wrapText: true, horizontal: 'center' };
-                }
-            });
+    filteredChildren.forEach((value, index) => {
+      const row = worksheet.insertRow(insertRowIndex + index, [
+        index + 1,
+        `${value.surname} ${value.name} ${value.secondName}`,
+        value.dateOfBirth,
+        value.sportsCategory,
+        "Атлант — Домодедово",
+        "Якуш Т. Д.",
+      ]);
 
-            // Выровнять первую ячейку по центру
-            const firstCell = row.getCell(2);
-            firstCell.alignment = { horizontal: 'left' };
-        });
+      row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
+        if (colNumber !== 1) {
+          cell.alignment = { wrapText: true, horizontal: "center" };
+        }
+      });
 
-        // Генерация имени файла и запись его в директорию
-        const fileName = `dispancer_${dispancer}.xlsx`;
-        const filePath = path.join(dirPath, fileName);
-        await workbook.xlsx.writeFile(filePath);
+      // Выровнять первую ячейку по центру
+      const firstCell = row.getCell(2);
+      firstCell.alignment = { horizontal: "left" };
+    });
 
-        // Возвращаем путь к файлу
-        return filePath;
-    } catch (error) {
-        throw new Error(`Error creating Excel file for ${dispancer}: ${error.message}`);
-    }
+    // Генерация имени файла и запись его в директорию
+    const fileName = `dispancer_${dispancer}.xlsx`;
+    const filePath = path.join(dirPath, fileName);
+    await workbook.xlsx.writeFile(filePath);
+
+    // Возвращаем путь к файлу
+    return filePath;
+  } catch (error) {
+    throw new Error(
+      `Error creating Excel file for ${dispancer}: ${error.message}`
+    );
+  }
 }
